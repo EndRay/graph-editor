fa56e46999fb7b0eedc3ee29de2c09bc75a55edf
EndRay <a.i.kovrigin@gmail.com>
2023-11-28 08:47:20 +0100

    Better code. Nodes and edges can be removed.

--- pygraphedit/edit.py
+++ pygraphedit/edit.py
@@ -6,11 +6,13 @@ import numpy as np
 import networkx as nx
 from scipy.spatial import distance
 
-def edit(G: nx.Graph):
-    vert_coordinates = np.empty((0, 2))
+from pygraphedit.visual_graph import VisualGraph
+
+
+def edit(graph: nx.Graph):
+    visual_graph = VisualGraph(graph, (800, 500))
 
     vert_tool = True
-    selected_node = -1
 
     # creating toolbox
     tools = widgets.RadioButtons(
@@ -19,14 +21,18 @@ def edit(G: nx.Graph):
         description='Tools:'
     )
 
+    debug_text = widgets.Textarea(
+        value='',
+        placeholder='Debug',
+    )
+
     def tool_change(change):
         nonlocal vert_tool
-        nonlocal selected_node
         if change['type'] != 'change' or change['name'] != 'value':
             return
         elif change['new'] == 'Vertices':
             vert_tool = True
-            selected_node = -1
+            visual_graph.selected_node = None
         else:
             vert_tool = False
 
@@ -38,50 +44,73 @@ def edit(G: nx.Graph):
 
     # creating canvas
     canvas = Canvas(width=800, height=500)
-    canvas.stroke_rect(0, 0, 800, 500)
 
     # click event handling
-    d = Event(source=canvas, watched_events=['click'])  # , 'keydown', 'mouseenter'
+    d = Event(source=canvas, watched_events=['click', 'drag'])  # , 'keydown', 'mouseenter'
 
-    def draw_vertex(x, y, colorcode="black"):
-        canvas.fill_style = colorcode
-        canvas.fill_circle(x, y, 3)
-        canvas.fill_style = "black"
+    def draw_graph(canvas: Canvas, visual_graph: VisualGraph):
+        def clear_canvas():
+            canvas.clear()
+            canvas.stroke_style = "black"
+            canvas.stroke_rect(0, 0, 800, 500)
+
+        def draw_vertex(pos, colorcode="black"):
+            canvas.fill_style = colorcode
+            canvas.fill_circle(pos[0], pos[1], 5)
+
+        def draw_edge(pos1, pos2, colorcode="black"):
+            canvas.stroke_style = colorcode
+            canvas.line_width = 2
+            canvas.stroke_line(*pos1, *pos2)
+
+        clear_canvas()
+        for node1, node2 in visual_graph.graph.edges:
+            draw_edge(visual_graph.coordinates[node1], visual_graph.coordinates[node2])
+        for node, pos in visual_graph.coordinates.items():
+            if node == visual_graph.selected_node:
+                draw_vertex(pos, "red")
+            else:
+                draw_vertex(pos)
+
+    def mex(arr):
+        result = 0
+        while result in arr:
+            result += 1
+        return result
 
     def handle_event(event):
-        nonlocal G
-        nonlocal vert_coordinates
         ev_x = event['relativeX']
         ev_y = event['relativeY']
+        pos = (ev_x, ev_y)
         if vert_tool:
-            vert_coordinates = np.append(vert_coordinates, [[ev_x, ev_y]], axis=0)
-            draw_vertex(ev_x, ev_y)
-            G.add_node(G.number_of_nodes(), x=ev_x, y=ev_y)
+            clicked_node, dist = visual_graph.get_closest_node(pos)
+            if dist < 10:
+                visual_graph.remove_node(clicked_node)
+            else:
+                visual_graph.add_node(mex(visual_graph.graph.nodes), pos)
         else:
-            nonlocal selected_node
-            clicked_node = distance.cdist(np.array([[ev_x, ev_y]]), vert_coordinates).argmin()
+            clicked_node = visual_graph.get_closest_node(pos)[0]
 
-            if selected_node == -1:
-                selected_node = clicked_node
-                draw_vertex(*vert_coordinates[selected_node], "blue")
+            if visual_graph.selected_node is None:
+                visual_graph.selected_node = clicked_node
 
-            elif selected_node == clicked_node:
-                draw_vertex(*vert_coordinates[selected_node])
-                selected_node = -1
+            elif visual_graph.selected_node == clicked_node:
+                visual_graph.selected_node = None
 
             else:
-                G.add_edge(selected_node, clicked_node)
-                canvas.stroke_line(*vert_coordinates[selected_node], *vert_coordinates[clicked_node])
+                if visual_graph.graph.has_edge(visual_graph.selected_node, clicked_node):
+                    visual_graph.remove_edge(visual_graph.selected_node, clicked_node)
+                else:
+                    visual_graph.add_edge(visual_graph.selected_node, clicked_node)
+                visual_graph.selected_node = None
+        draw_graph(canvas, visual_graph)
 
-                draw_vertex(*vert_coordinates[clicked_node])
-                draw_vertex(*vert_coordinates[selected_node])
-                selected_node = -1
-
+    draw_graph(canvas, visual_graph)
     d.on_dom_event(handle_event)
 
     # main widget view
     main_box = widgets.HBox(
-        [toolbox, canvas]
+        [widgets.VBox([toolbox, debug_text]), canvas]
     )
     # Display the widgets
     display(main_box)
---
+++ pygraphedit/visual_graph.py
@@ -0,0 +1,45 @@
+import random
+
+import networkx as nx
+import numpy as np
+
+
+class VisualGraph:
+    def __init__(self, graph: nx.Graph, bounds: (int, int)):
+        self.graph = graph
+        self.bounds = bounds
+        self.coordinates = {
+            node: np.array([random.randint(0, bounds[0]-1),
+                            random.randint(0, bounds[1]-1)])
+            for node in graph.nodes
+        }
+        self.selected_node = None
+
+    def add_node(self, node, pos: (int, int)):
+        self.graph.add_node(node)
+        self.coordinates[node] = pos
+
+    def add_edge(self, node1, node2):
+        self.graph.add_edge(node1, node2)
+
+    def remove_node(self, node):
+        self.graph.remove_node(node)
+        del self.coordinates[node]
+
+    def remove_edge(self, node1, node2):
+        self.graph.remove_edge(node1, node2)
+
+    def move_vertex(self, node, pos: (int, int)):
+        if node not in self.graph.nodes:
+            raise ValueError("Node not in graph")
+        self.coordinates[node] = pos
+
+    def get_closest_node(self, pos: (int, int)) -> (any, float):
+        closest_node = None
+        closest_dist = float("inf")
+        for node, node_pos in self.coordinates.items():
+            dist = np.linalg.norm(np.array(pos) - node_pos)
+            if dist < closest_dist:
+                closest_dist = dist
+                closest_node = node
+        return closest_node, closest_dist
